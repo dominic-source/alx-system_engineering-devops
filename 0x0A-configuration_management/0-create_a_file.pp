6e86194c4359851b96bb5566a300f615950e99b6
dominic-source <chinonsodomnic@gmail.com>
2023-09-22 14:48:18 -0700

    puppet practice

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+file { '/tmp':
+  ensure  => directory,
+}
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
