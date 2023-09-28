36fc9bce01882251ec7e7a0ee157e4d6cfd6a6bc
dominic-source <chinonsodomnic@gmail.com>
2023-09-28 11:52:22 +0000

    debugging

--- 0x0C-web_server/7-puppet_install_nginx_web_server2.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server2.pp
@@ -4,8 +4,6 @@
 package { 'nginx':
   ensure  => installed,
 }
-
-# Create directory structure
 file { '/var/www/index.com':
   ensure  => directory,
 }
@@ -14,17 +12,17 @@ file { '/var/www/index.com/html':
   ensure  => directory,
 }
 
-# Create HTML content file
+# create html content file
 file { '/var/www/index.com/html/index.html':
   ensure  => file,
   mode    => '0755',
   content => 'Hello World!',
 }
 
-# Declare site configuration path
+# declare site configuration path
 $site = '/etc/nginx/sites-available/index2.com'
 
-# Duplicate configuration file
+# duplicate configuration file
 file { $site:
   ensure => present,
   source => '/etc/nginx/sites-available/default',
@@ -36,17 +34,22 @@ file { '/etc/nginx/sites-enabled/index2.com':
   target => $site,
 }
 
+# deletes default file in sites-enabled
+#file { '/etc/nginx/sites-enabled/default':
+#  ensure  => absent,
+#}
+
 # Edit configuration file
 file_line { 'edit_conf_hash_buck':
   ensure => present,
-  line   => '   server_names_hash_bucket_size 64;',
+  line   => '	server_names_hash_bucket_size 64;',
   match  => '# server_names_hash_bucket_size 64;',
   path   => '/etc/nginx/nginx.conf',
 }
 
 file_line { 'edit site configuration file':
   ensure   => present,
-  line     => ' server_name index2.com www.index2.com;',
+  line     => '	server_name index2.com www.index2.com;',
   match    => 'server_name _;',
   path     => $site,
   multiple => false,
@@ -54,17 +57,16 @@ file_line { 'edit site configuration file':
 
 file_line { 'edit site html configuration file':
   ensure   => present,
-  line     => ' root /var/www/index.com/html;',
+  line     => '	root /var/www/index.com/html;',
   match    => 'root /var/www/html;',
   path     => $site,
   multiple => false,
 }
 
-# Add location block for redirection
 file_line { 'edit site redirect configuration':
   ensure   => present,
-  line     => ' location /redirect_me { return 301 https://google.com; }',
-  match    => '^# location / {',
+  line     => "\n\tlocation /redirect_me {\n\t\treturn 301 https://google.com;\n\t}\n\n\tlocation / {",
+  match    => '^\s+location \/ {',
   path     => $site,
   multiple => false,
 }
@@ -72,4 +74,3 @@ file_line { 'edit site redirect configuration':
 service { 'nginx':
   ensure  => 'running',
 }
-
