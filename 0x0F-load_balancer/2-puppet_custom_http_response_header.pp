ec58f68928003e4c112bbcd253efbce3594ea13c
dominic-source <chinonsodomnic@gmail.com>
2023-10-03 14:47:06 -0700

    debug some version

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,17 +8,15 @@ package { 'nginx':
   ensure  => installed,
 }
 file { '/var/www/html/index.html':
-  content => 'Holberton School!',
+  content => 'The Holberton School!',
 }
 
 # Edit index.com site config
-$line2 = "\tadd_header X-Served-By ${hostname};"
-$match2 = 'server_name _;'
 file_line { 'edit config file and add header':
-  ensure  => present,
-  path    => "/etc/nginx/sites-available/default",
-  line    => ${line2},
-  after   => ${match2},
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => '\tadd_header X-Served-By ${hostname};',
+  after  => 'server_name _;',
 }
 
 service { 'nginx':
