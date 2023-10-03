ce6ec0c2870e9a6b21145f3a799f20d9ec24761d
dominic-source <chinonsodomnic@gmail.com>
2023-10-03 16:17:11 -0700

    debugging task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,14 +1,12 @@
 # Install nginx and configure the server
 
 # Ensures that nginx is installed
--> package { 'nginx':
+package { 'nginx':
   ensure  => installed,
 }
 
 # create html content file
--> file { '/var/www/html/index.html':
-  ensure  => file,
-  mode    => '0755',
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
@@ -16,24 +14,25 @@
 $site = '/etc/nginx/sites-available/index2.com'
 
 # duplicate configuration file
--> file { $site:
+file { $site:
   ensure => present,
   source => '/etc/nginx/sites-available/default',
 }
 
 # Link index2.com file to sites-enabled folder
--> file { '/etc/nginx/sites-enabled/index2.com':
+file { '/etc/nginx/sites-enabled/index2.com':
   ensure => link,
   target => $site,
 }
 
--> file_line { 'edit site redirect configuration':
+file_line { 'edit site redirect configuration':
   ensure => present,
   path   => $site,
   line   => 'rewrite ^/redirect_me https://google.com permanent;',
-  after  => 'liston 80 default_server;',
+  after  => 'listen 80 default_server;',
 }
 
--> service { 'nginx':
+service { 'nginx':
   ensure  => 'running',
+  require => Package['nginx'],
 }
