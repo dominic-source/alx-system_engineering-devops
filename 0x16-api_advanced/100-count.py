509c8552e20f1739b66cab515138a7285857d292
dominic-source <chinonsodomnic@gmail.com>
2023-11-07 14:32:17 -0800

    count language

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,16 +6,17 @@ product
 import requests
 import sys
 
-    
+
 def count_words(subreddit, word_list, hot_object={}, start=True, after=''):
     """count all hot post"""
     url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
                                                                 subreddit,
                                                                 after)
-    header = {'User-Agent': 'chinns comp'}
+    header = {'User-Agent': 'adaDASS'}
+
     res = requests.get(url, allow_redirects=False, headers=header)
     result = None
-    words = [word.lower for word in  word_list]
+    words = [word.lower for word in word_list]
     if res.status_code == requests.codes.ok:
         result = res.json()
         for child in result.get('data').get('children'):
@@ -23,18 +24,21 @@ def count_words(subreddit, word_list, hot_object={}, start=True, after=''):
             title_low = title.lower()
             if title_low in words:
                 for data in words:
-                    if data == title_low:
+                    if data in title_low.split():
                         hot_object[data] += hot_object.get(data, 0)
         next_page = result.get('data').get('after')
-        count_words(subreddit, word_list, hot_object, start=False, after=next_page)
+        count_words(subreddit, word_list, hot_object,
+                    start=False, after=next_page)
     elif result is None:
         return None
     if start:
         if hot_object:
-            sorted_object = {key: hot_object[key] for key in sorted(hot_objects) if hot_object[key] != 0}
+            sorted_object = {key: hot_object[key]
+                             for key in sorted(hot_objects)
+                             if hot_object[key] != 0}
             print(sorted_object)
         else:
-            print('i got here')
+            print
 
 
 if __name__ == '__main__':
