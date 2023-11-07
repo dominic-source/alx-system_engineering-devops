171c281d0c0dd72c08b2b018046832e1d57fba25
dominic-source <chinonsodomnic@gmail.com>
2023-11-07 10:40:14 -0800

    debug recursion task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,12 +7,12 @@ import requests
 import sys
 
 
-def recurse(subreddit, hot_list=[], after=""):
+def recurse(subreddit, hot_list=[], after="", start=True):
     """count all hot post"""
     url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
                                                                 subreddit,
                                                                 after)
-    header = {'User-Agent': 'web api client/1.0'}
+    header = {'User-Agent': 'it is v/1.1'}
     res = requests.get(url, allow_redirects=False, headers=header)
     result = None
     if res.status_code == requests.codes.ok:
@@ -20,9 +20,11 @@ def recurse(subreddit, hot_list=[], after=""):
         for title in result.get('data').get('children'):
             hot_list.append(title.get('data').get('title'))
         next_page = result.get('data').get('after')
-        recurse(subreddit, hot_list, next_page)
+        recurse(subreddit, hot_list, next_page, start=False)
     elif result is None or not hot_list:
         return None
+    if start:
+        return hot_list
 
 
 if __name__ == '__main__':
