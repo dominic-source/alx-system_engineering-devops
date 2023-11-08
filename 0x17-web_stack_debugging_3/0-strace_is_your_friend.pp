2a7f9c8149cb663700f3017e2df5013efcb8f79c
Dominic <chinonsodomnic@gmail.com>
2023-11-09 00:01:02 +0100

    best Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,5 @@
 # debug the apache2 server
 exec { 'modify wp-setting file':
-  command => "sed -i '137s|phpp|php|' '/var/www/html/wp-settings.php'",
+  command => "sudo sed -i '137s|phpp|php|' '/var/www/html/wp-settings.php'",
   path    => ['/bin', '/usr/bin', '/usr/sbin'],
-}
-service { 'apache2':
-  ensure  => 'running',
 }
