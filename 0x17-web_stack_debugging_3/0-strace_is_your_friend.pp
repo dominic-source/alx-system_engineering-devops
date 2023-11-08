175e4250e748a69b7ad46a7283957a95acd2cf33
dominic-source <chinonsodomnic@gmail.com>
2023-11-08 11:48:00 -0800

    debug debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # debug the apache2 server
 exec { 'modify wp-setting file':
-  command => "sed -i '137s|phpp|php|' '/var/www/html/wp-settings.php'",
+  command => "sed -i '138s|phpp|php|' '/var/www/html/wp-settings.php'",
   path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
